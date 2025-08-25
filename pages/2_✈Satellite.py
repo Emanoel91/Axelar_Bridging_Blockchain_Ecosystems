f004408fc51115e0d6df1166e9be66a9ee277113
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-25 23:56:14 +0330

    Update 2_✈Satellite.py

--- pages/2_✈Satellite.py
+++ pages/2_✈Satellite.py
@@ -15,7 +15,7 @@ st.set_page_config(
 )
 
 # --- Title  -----------------------------------------------------------------------------------------------------
-st.title("📐Metrics")
+st.title("✈Satellite")
 
 # --- attention ---------------------------------------------------------------------------------------------------------
 st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
