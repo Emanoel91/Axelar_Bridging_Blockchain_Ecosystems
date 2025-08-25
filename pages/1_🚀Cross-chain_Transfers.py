aa9115e7635514185509e7a09f6998dadc2d8baf
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-25 12:13:09 +0330

    Update 1_🚀Cross-chain_Transfers.py

--- pages/1_🚀Cross-chain_Transfers.py
+++ pages/1_🚀Cross-chain_Transfers.py
@@ -438,7 +438,7 @@ with col4:
 with col5:
     st.metric(
         "Top Destination Chain by Number of Destination Chains",
-        f"{top_dest_chains['📥Destination Chain']} ({top_dest_chains['📤#Source Chains']:,})"
+        f"{top_source_chains['📥Destination Chain']} ({top_source_chains['📤#Source Chains']:,})"
     )
 with col6:
     st.metric(
